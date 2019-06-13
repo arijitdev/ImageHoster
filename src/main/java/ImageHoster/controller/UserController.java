ff5d0f6edb0895a6bb180d07b56125061703ee4c
Arijit Deb <arijitdeb.work@gmail.com>
2019-06-14 05:16:42 +0800

    Updating Real User validation from Database

--- src/main/java/ImageHoster/controller/UserController.java
+++ src/main/java/ImageHoster/controller/UserController.java
@@ -16,34 +16,44 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    //This controller method is called when the request pattern is of type 'users/registration'
+    //This method declares User type and UserProfile type object
+    //Sets the user profile with UserProfile type object
+    //Adds User type object to a model and returns 'users/registration.html' file
     @RequestMapping("users/registration")
     public String registration(Model model) {
-    	User user = new User();
- 	   UserProfile profile = new UserProfile();
- 	   user.setProfile(profile);
-
- 	   model.addAttribute("User", user);
-
- 	   return "users/registration";
+        User user = new User();
+        UserProfile profile = new UserProfile();
+        user.setProfile(profile);
+        model.addAttribute("User", user);
+        return "users/registration";
     }
 
-   @RequestMapping(value = "users/registration", method = RequestMethod.POST)
+    //This controller method is called when the request pattern is of type 'users/registration' and also the incoming request is of POST type
+    //This method calls the business logic and after the user record is persisted in the database, directs to login page
+    @RequestMapping(value = "users/registration", method = RequestMethod.POST)
     public String registerUser(User user) {
-    	userService.registerUser(user);
- 	    return "users/login";
+        userService.registerUser(user);
+        return "redirect:/users/login";
     }
 
+    //This controller method is called when the request pattern is of type 'users/login'
     @RequestMapping("users/login")
     public String login() {
         return "users/login";
     }
 
+    //This controller method is called when the request pattern is of type 'users/login' and also the incoming request is of POST type
+    //The return type of the business logic is changed to User type instead of boolean type. The login() method in the business logic checks whether the user with entered username and password exists in the database and returns the User type object if user with entered username and password exists in the database, else returns null
+    //If user with entered username and password exists in the database, direct to user homepage displaying all the images in the application
+    //If user with entered username and password does not exist in the database, redirect to the same login page
     @RequestMapping(value = "users/login", method = RequestMethod.POST)
     public String loginUser(User user) {
-        boolean userExists = userService.login(user);
-        if (userExists) {
+    	User existingUser=userService.login(user);
+        if(existingUser!=null) {
             return "redirect:/images";
-        } else {
+        }
+        else {
             return "users/login";
         }
     }
