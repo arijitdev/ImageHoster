c1ba229ba1df75652fa5311c397e2149294c627c
Arijit Deb <arijitdeb.work@gmail.com>
2019-06-14 08:51:53 +0800

    Fetching real image from the database and displaying it to particular logged in user

--- src/main/java/ImageHoster/repository/ImageRepository.java
+++ src/main/java/ImageHoster/repository/ImageRepository.java
@@ -3,10 +3,8 @@ package ImageHoster.repository;
 import ImageHoster.model.Image;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.PersistenceUnit;
+import javax.persistence.*;
+import java.util.List;
 
 //The annotation is a special type of @Component annotation which describes that the class defines a data repository
 @Repository
@@ -23,18 +21,54 @@ public class ImageRepository {
     //The transaction is committed if it is successful
     //The transaction is rolled back in case of unsuccessful transaction
     public Image uploadImage(Image newImage) {
-        //Complete the method
-    	EntityManager em = emf.createEntityManager();
-    	EntityTransaction transaction = em.getTransaction();
-    	
-    	try {
- 	       transaction.begin();
- 	       em.persist(newImage);
- 	       transaction.commit();
- 	   }catch(Exception e) {
- 	       transaction.rollback();
- 	   }
 
- 	   return newImage;
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+
+        try {
+            transaction.begin();
+            em.persist(newImage);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+        }
+        return newImage;
     }
+
+    //The method creates an instance of EntityManager
+    //Executes JPQL query to fetch all the images from the database
+    //Returns the list of all the images fetched from the database
+    public List<Image> getAllImages() {
+         //Complete the code
+    	 EntityManager em = emf.createEntityManager();
+         TypedQuery<Image> query = em.createQuery("SELECT i from Image i", Image.class);
+         List<Image> resultList = query.getResultList();
+         
+         return resultList;
+         
+    }
+
+    //The method creates an instance of EntityManager
+    //Executes JPQL query to fetch the image from the database with corresponding title
+    //Returns the image in case the image is found in the database
+    //Returns null if no image is found in the database
+    public Image getImageByTitle(String title) {
+    	
+       try {
+			EntityManager em = emf.createEntityManager();
+		
+			TypedQuery<Image> typedQuery=em.createQuery("SELECT i FROM Image i WHERE i.title = :title",Image.class);
+		
+			typedQuery.setParameter("title", title);
+		
+			return typedQuery.getSingleResult();
+		
+		}catch(NoResultException e) {
+			
+			return null;
+		
+		}
+       
+    }
+
 }
